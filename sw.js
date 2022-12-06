5aa31bca7b5c3ca6abc02b6aef5bb81c540cd6b4
Diego Maldonado Amaya <190495@utags.edu.mx>
2022-12-05 22:59:25 -0600

    terminado

--- sw.js
+++ sw.js
@@ -1,71 +1,45 @@
-//asignar nombre y version de cache
-const CACHE_NAME = 'v1_cache_mrnm';
-
-//configurar los archivos del cache
-
-var urlToCache = [
-    './',
-    './css/style.css',
-    './images/1.png',
-    './images/2.png',
-    './images/3.png',
-    './images/4.png',
-    './images/5.png',
-    './images/facebook.png',
-    './images/instagram.png',
-    './images/twiter.png',
-];
-
-// Evento install
-// Instalacion del service worker y guardar en cache los recursos estaticos
-self.addEventListener('install', e =>{
-    e.waitUntil(
-        caches.open(CACHE_NAME)
-              .then(cache => {
-                    return cache.addAll(urlToCache)
-                           .then(() => {
-                                self.skipWaiting();
-                           })
-                 })
-                 .catch(err => {
-                    console.log('No se ha registrado el cache', err);
-                   })
-    )
+self.addEventListener('install', e=>{
+    caches.open('cache-v1')
+    .then(cache =>{
+        cache.addAll([
+            './',
+            'css/style.css',
+            'images/facebook.png',
+            'images/instagram.png',
+            'images/twiter.png',
+            'main.js',
+            'images/logo.jpg',
+            'images/nike1.jpg',
+            'images/nike2.jpg',
+            'images/nike3.jpg',
+            'images/video1.mp4',
+            'images/video2.mp4',
+            'images/video3.mp4',
+            
+        ])
+    });
+    e.waitUntil(cacheProm);
 });
 
-//Evento activate
-self.addEventListener('activate', e =>{
-    const cacheWhiteList = [CACHE_NAME];
-    e.waitUntil(
-        caches.keys()
-              .then(cacheNames => {
-                return Promise.all(
-                    cacheNames.map(cacheName => {
-                        if(cacheWhiteList.indexOf(cacheName) === -1){
-                            // Borrar los elementos q no se necesitan
-                            return caches.delete(cacheName);
-                        }
-                    })
-                );
-              })
-              .then(() =>{
-                //activar la cache
-                self.clients.claim();
-              })
-    );
+self.addEventListener('fetch', e =>{
+    //cache with network fallback
+    const respuesta = caches.match( e.request )
+        .then ( res => {
+            if ( res ) return res;
+            //no existe el archivo
+            //tengo que ir a la web
+            console.log('No existe', e.request.url);
+            return fetch( e.request ).then ( newResp => {
+                caches.open('cache-v1')
+                    .then( cache => {
+                        cache.put( e.request, newResp);
+                    }
+
+                    )
+                return newResp.clone;
+            });
+        });
+        e.respondWith(respuesta);
+    //only cache
+    //e.respondWith( caches.match(e.request));
 });
-
-//Evento fetch
-
-self.addEventListener('fetch', e => {
-    e.respondWith(
-        caches.match(e.request)
-            .then(res => {
-                if(res){
-                    //devuelvo los datos desde cache
-                    return res;
-                }
-                return fetch(e.request);
-            })
-    );
-})
